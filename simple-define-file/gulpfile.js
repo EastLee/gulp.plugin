80e9692e8375a6ce4341dbb9feb86221a874d86e
lidong06 <lidong@192.168.0.105>
2016-11-06 13:27:43 +0800

    simple-define-file/

--- simple-define-file/gulpfile.js
+++ simple-define-file/gulpfile.js
@@ -15,8 +15,16 @@ const del = require('del');
 const mkdirp = require('mkdirp');
 const connect = require('gulp-connect');
 const opn = require('opn');
+const zip = require('gulp-zip');
+const sftp = require('gulp-sftp');
 
 var dirPaths = {
+    devPath: './src',
+    proPath: './dist',
+    zip: {
+        path: './zip',
+        name: 'dist.zip'
+    },
     html: './html',
     css: {
         src: './src/css',
@@ -30,21 +38,31 @@ var dirPaths = {
         src: './src/img',
         dist: './dist/img'
     },
-    mock:'./mock',
+    mock: './mock',
     doc: './doc',
     tool: './tool'
-}
+};
 
 var serverConfig = {
-    root: './',//设置根目录
-    livereload: true,//启动实施监控
-    port: 8888,//设置端口
-    name: 'Dev App',//设置服务器名字
-    domain:'localhost'
+    root: './', //设置根目录
+    livereload: true, //启动实施监控
+    port: 8888, //设置端口
+    name: 'Dev App', //设置服务器名字
+    domain: 'localhost'
+};
+
+var uploadConfig = {
+    proPath:'',
+    host: 'website.com',
+    user: 'johndoe',
+    pass: '1234',
+    port: 22,
+    remotePath: '/',
+    // timeout:''
 }
 
 //创建开发目录
-gulp.task('init', function() {
+gulp.task('g-init', function() {
     var dirs = [dirPaths.html, dirPaths.css.src, dirPaths.js.src, dirPaths.doc, dirPaths.img.src, dirPaths.tool];
     dirs.forEach(dir => {
         mkdirp.sync(dir);
@@ -53,7 +71,6 @@ gulp.task('init', function() {
 
 //处理css文件
 gulp.task('g-css', function() {
-
     return gulp.src(dirPaths.css.src + '/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
@@ -73,10 +90,10 @@ gulp.task('g-image', function() {
 });
 
 //处理css和img
-gulp.task('g-style',['g-css','g-image'],function(){
+gulp.task('g-style', ['g-css', 'g-image'], function() {
     gulp.src(dirPaths.css.dist + '/**/*.css')
         .pipe(spriter({
-            'spriteSheet': dirPaths.img.dist+'/spritesheet.png',
+            'spriteSheet': dirPaths.img.dist + '/spritesheet.png',
             'pathToSpriteSheetFromCSS': '../img/spritesheet.png'
         }))
         .pipe(gulp.dest(dirPaths.css.dist));
@@ -88,43 +105,54 @@ gulp.task('g-js', function() {
     gulp.src(dirPaths.js.src + '/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter(stylish))
-        .pipe(uglify())//有一定的语法检测能力，如果语法错的太离谱，这里可能会报错
+        .pipe(uglify()) //有一定的语法检测能力，如果语法错的太离谱，这里可能会报错
         .pipe(gulp.dest(dirPaths.js.dist));
 });
 
 
 //删除生成文件
-gulp.task('del',function(){
-    del(['./dist']).then(function(){
+gulp.task('g-del', function() {
+    del([proPath]).then(function() {
         console.info('delete success!');
     })
 });
 
-gulp.task('openbrowser', function() {
-  opn( 'http://' + serverConfig.domain + ':' + serverConfig.port );
+gulp.task('g-openbrowser', function() {
+    opn('http://' + serverConfig.domain + ':' + serverConfig.port);
 });
 
-gulp.task('connect', function() {
+gulp.task('g-connect', function() {
     connect.server({
-        root: serverConfig.root,//设置根目录
-        livereload: serverConfig.livereload,//启动实施监控
-        port: serverConfig.port,//设置端口
-        name: serverConfig.name//设置服务器名字
+        root: serverConfig.root, //设置根目录
+        livereload: serverConfig.livereload, //启动实施监控
+        port: serverConfig.port, //设置端口
+        name: serverConfig.name //设置服务器名字
     });
 });
 
-gulp.task('g-reload',function(){
-     gulp.src('./src/**/*.*')
-    .pipe(connect.reload())
+gulp.task('g-reload', function() {
+    gulp.src(dirPaths.devPath + '/**/*.*')
+        .pipe(connect.reload())
 })
 
 //监听
-gulp.task('g-watch',function(){
+gulp.task('g-watch', function() {
     //watch 只能监听现存的文件对于新建文件 无法监听
-    gulp.watch(dirPaths.html+'/**/*.html',['g-reload']);
-    gulp.watch(dirPaths.js.src+'/**/*.js',['g-reload','g-js']);
-    gulp.watch(dirPaths.css.src+'/**/*.scss',['g-reload','g-style']);
+    gulp.watch(dirPaths.html + '/**/*.html', ['g-reload']);
+    gulp.watch(dirPaths.js.src + '/**/*.js', ['g-reload', 'g-js']);
+    gulp.watch(dirPaths.css.src + '/**/*.scss', ['g-reload', 'g-style']);
 });
 
 
-gulp.task('default',['connect','g-watch','openbrowser'])
+gulp.task('default', ['g-connect', 'g-watch', 'g-openbrowser']);
+
+gulp.task('g-zip', function() {
+    return gulp.src(dirPaths.proPath + '/**/*.*')
+        .pipe(zip(dirPaths.zip.name))
+        .pipe(gulp.dest(dirPaths.zip.path));
+});
+
+gulp.task('g-upload', function() {
+    return gulp.src(uploadConfig.proPath)
+        .pipe(sftp(uploadConfig));
+});
