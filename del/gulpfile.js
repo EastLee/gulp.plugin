f393ad92bbdfc0933508570359ddc4452394d4ee
lidong06 <lidong@192.168.0.107>
2016-12-25 19:29:26 +0800

    modify del

--- del/gulpfile.js
+++ del/gulpfile.js
@@ -1,6 +1,19 @@
 var gulp = require('gulp');
 const del = require('del');
 
+var args = process.argv.slice(2);
+console.info(args);
+
+function test(){
+    console.info('test!');
+}
+
+test();
+
+gulp.task('default', function() {
+    test();
+});
+
 //删除文件
 gulp.task('delFile', function() {
     del(['tmp/*.js', '!tmp/a.js'],{dryRun: false}).then(paths => {
